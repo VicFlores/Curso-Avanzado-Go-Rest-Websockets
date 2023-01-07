0e6469113bf58d1169b88aa4d51b5512d6dbc9ec
Vic Flores <vicsito2014@gmail.com>
2023-01-07 11:24:05 -0600

    First commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"go/avanzado/rest-ws/handlers"
+	"go/avanzado/rest-ws/server"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalln("Error loading .env file:", err)
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:   JWT_SECRET,
+		Port:        PORT,
+		DatabaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s.Start(BindRoutes)
+
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
